12eb045d0d22dbab07b7fecb03fa01df564c2f3d
Tony Tran <vnmasaz@gmail.com>
2022-05-28 11:33:00 +0200

    One import.js to import everything

--- js/footer.js
+++
@@ -1,9 +0,0 @@
-fetch('components/footer.html')
-    .then(result => result.text())
-    .then(text => {
-        let old_element = document.querySelector("script#footer");
-        let new_element = document.createElement("footer");
-        new_element.className = "site-footer";
-        new_element.innerHTML = text;
-        old_element.parentNode.replaceChild(new_element,old_element);
-    })
---
+++ js/import.js
@@ -0,0 +1,29 @@
+fetch('components/navbar.html')
+    .then(result => result.text())
+    .then(text => {
+            let old_element = document.querySelector("div#navbar");
+            let new_element = document.createElement("div");
+            new_element.className = "site-wrap";
+            new_element.innerHTML = text;
+            old_element.parentNode.replaceChild(new_element,old_element);
+
+            let current_page = window.location.pathname.split("/").slice(2).join("/");
+            for (let menu of new_element.querySelectorAll("ul.site-menu")) {
+                    let items = menu.getElementsByTagName("li");
+                    for (let i = 0; i < items.length; ++i) {
+                            if (current_page === items[i].getElementsByTagName("a")[0].getAttribute("href")) {
+                                    items[i].classList.add("active");
+                            }
+                    }
+            }
+    })
+
+fetch('components/footer.html')
+    .then(result => result.text())
+    .then(text => {
+        let old_element = document.querySelector("div#footer");
+        let new_element = document.createElement("footer");
+        new_element.className = "site-footer";
+        new_element.innerHTML = text;
+        old_element.parentNode.replaceChild(new_element,old_element);
+    })
